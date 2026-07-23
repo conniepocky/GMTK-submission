022571fab2f720b8b63be548aa4f4df416ff172a
Connie <c.amer@icloud.com>
2026-07-23 13:01:50 +0100

    maths minigame and humming invisible button

--- game_scene.gd
+++ game_scene.gd
@@ -1,22 +1,23 @@
 extends Node2D
 
-var current_clicks: int = 0
-var current_state = 0 
+var current_clicks: int = 1
+var current_state = 0
 
 var current_block: Button = null
 var drag_offset: Vector2 = Vector2.ZERO
+
+const PONG_SCENE = preload("res://scenes/pong_minigame.tscn")
 
 #state 0 1-10 normal clicking
 #state 1 11-25 button moves away
 #state 2 26-40 fake buttons that subtract points
 #state 3 41-50 sort numbers 
 #state 4 51-60 maths questions to increment 
-#state 5 61 fake crash, close button increments 
-#state 6 62-72 humming invisible button 
-#state 7 73-85 bouncing button
-#state 8 86-94 error type in commands
-#state 9 95-98 button locked in centre dodge bullets
-#state 10 99 button shatters, reassamble for 100 
+#state 5 61-72 humming invisible button 
+#state 6 73-84 pong 
+#state 7 85-94 text adventure
+#state 8 95-98 button locked in centre dodge bullets
+#state 9 final click
 
 @onready var label = $Counter
 @onready var button = $Button
@@ -28,8 +29,25 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	
+	# update dragging 
 	if current_block != null:
 		current_block.position = get_global_mouse_position() - drag_offset
+
+	# update sound based on distance from button
+	
+	if current_state == 5:
+		var btn_center = button.position + (button.size / 2)
+		var distance = get_global_mouse_position().distance_to(btn_center)
+		
+		var max_distance = 800.0 
+		
+		var linear_volume = 1.0 - clamp(distance / max_distance, 0.0, 1.0)
+		
+		if linear_volume > 0.01:
+			hum_player.volume_db = linear_to_db(linear_volume)
+		else:
+			hum_player.volume_db = -80.0 # mute
 
 func update_state_logic() -> void:
 	if current_clicks >= 11 and current_clicks <= 25:
@@ -37,18 +55,36 @@ func update_state_logic() -> void:
 	elif current_clicks >= 26 and current_clicks <= 40:
 		current_state = 2 
 	elif current_clicks >= 41 and current_clicks <= 50:
-		clear_fake_buttons()
+		clear_fake_buttons() #clean up state 2
 		current_state = 3
 		spawn_sorting_puzzle()
+	elif current_clicks >= 51 and current_clicks <= 60:
+		current_state = 4
+	elif current_clicks >= 61 and current_clicks <= 72:
+		maths_popup.hide() # clean up State 4
+		current_state = 5
+		start_invisible_button()
+	elif current_clicks == 73:
+		hum_player.stop() #clean up state 5
+		button.modulate.a = 1.0 # Make it visible again for the future
+		button.hide()
+		current_state = 6
+		   
+		start_pong_state()
 		
 func updateCounter() -> void:
 	label.text = str(current_clicks) + " / 100"
 
 func _on_button_pressed() -> void:
+	
+	if current_state == 4:
+		maths_popup.show()
+		load_next_maths_question()
+		return # stop standard clicking
+	
 	current_clicks += 1 
 	
 	updateCounter()
-	
 	update_state_logic()
 	
 	match current_state:
@@ -60,6 +96,8 @@ func _on_button_pressed() -> void:
 			pass
 			#move_button_randomly()
 			#spawn_fake_buttons(3)
+		5:
+			move_button_randomly()
 			
 func _on_fake_pressed() -> void:
 	current_clicks -= 1 
@@ -169,3 +207,66 @@ func check_sort_order() -> void:
 		
 		button.show()
 		button.position = get_viewport_rect().size / 2 
+
+#state 4
+
+@onready var maths_popup = $MathsPopup
+@onready var question_label = $MathsPopup/QuestionLabel
+@onready var question_image = $MathsPopup/QuestionImage
+@onready var answer_input = $MathsPopup/AnswerInput
+
+var current_maths_index: int = 0
+var maths_questions: Array = [
+	{"type": "text", "q": "2+2", "a": "4"},
+	{"type": "text", "q": "10+20", "a": "30"},
+	{"type": "text", "q": "8-3", "a": "5"},
+	{"type": "text", "q": "5x5", "a": "25"},
+	{"type": "text", "q": "3+3+3", "a": "9"},
+	{"type": "text", "q": "49 / 7", "a": "7"},
+	{"type": "text", "q": "12x12", "a": "144"},
+	{"type": "image", "q": "res://maths/eq.png", "a": "5"},
+	{"type": "image", "q": "res://maths/trig.png", "a": "1"},
+	{"type": "image", "q": "res://maths/int.png", "a": "1"},
+]
+
+func load_next_maths_question() -> void:
+	var current_q = maths_questions[current_maths_index]
+	
+	if current_q["type"] == "text":
+		question_label.show()
+		question_image.hide()
+		question_label.text = current_q["q"]
+	elif current_q["type"] == "image":
+		question_label.hide()
+		question_image.show()
+		question_image.texture = load(current_q["q"])
+
+func _on_submit_button_pressed() -> void:
+	var typed_answer = answer_input.text.strip_edges().to_lower()
+	var correct_answer = maths_questions[current_maths_index]["a"].to_lower()
+	
+	if typed_answer == correct_answer:
+		current_clicks += 1
+		updateCounter()
+		current_maths_index += 1
+		answer_input.text = "" 
+		
+		if current_maths_index < maths_questions.size() and current_clicks <= 60:
+			load_next_maths_question()
+		else:
+			maths_popup.hide()
+			update_state_logic()
+	else:
+		answer_input.text = ""
+
+#state 5
+
+@onready var hum_player = $HumPlayer
+
+func start_invisible_button() -> void:
+	button.show() 
+	button.modulate.a = 0.0 
+	hum_player.play()
+	move_button_randomly()
+	
+#state 6
